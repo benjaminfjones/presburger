8b20c15c19a4b3f63c3119044fc556bd06ca4a89
Benjamin Jones <benjaminfjones@gmail.com>
2025-06-28 23:14:10 +0000

    Remove juxtaposition multiplication in the parser and other parser and test improvements

--- tests/test_parser.rs
+++ tests/test_parser.rs
@@ -13,52 +13,52 @@ mod test_parser {
 
     #[test]
     fn test_numlit() {
-        let cases = vec!["222", "(222)", "((((222))))"];
+        let cases = vec!["222", "(222)", "((((222))))", "2/3", "(2/3)", "((-1/3))"];
         for c in cases {
-            assert!(grammer::TermParser::new().parse(c).is_ok(), "case: {}", c);
+            assert!(grammer::SumParser::new().parse(c).is_ok(), "case: {}", c);
         }
-        assert!(grammer::TermParser::new().parse("((22)").is_err());
+        assert!(grammer::SumParser::new().parse("((22)").is_err());
     }
 
     #[test]
     fn test_var() {
-        let cases = vec!["x", "(x)", "((((y))))", "2 z"];
+        let cases = vec!["x", "(x)", "((((y))))", "2 * z"];
         for c in cases {
-            assert!(grammer::TermParser::new().parse(c).is_ok(), "case: {}", c);
+            assert!(grammer::SumParser::new().parse(c).is_ok(), "case: {}", c);
         }
         // negative tests:
-        assert!(grammer::TermParser::new().parse("7 z 7").is_err());
+        assert!(grammer::SumParser::new().parse("7 * z * 7").is_err()); // TODO: not yet supported
+        assert!(grammer::SumParser::new().parse("x * z").is_err()); // non-linear
     }
 
     #[test]
-    fn test_term() {
+    fn test_sum() {
         let cases = vec![
-            "5",
-            "y",
             "x+1",
             "(x) + 1",
-            "(x + 1)",
             "y + x + 1",
-            "y + (x + 1) + z",
-            "1/3 x",
-            "x + -5/1 y + -1/-1 z",
+            "1/3 * x",
+            "(1/3) * x",
+            "(-1) * x + (-2/3)",
+            "x + -5/1 * y + -1/-1 * z",
         ];
         for c in cases {
-            assert!(grammer::TermParser::new().parse(c).is_ok(), "case: {}", c);
+            assert!(grammer::SumParser::new().parse(c).is_ok(), "case: {}", c);
         }
     }
 
     #[test]
     fn test_good_atoms() {
         let cases = vec![
-            "@T",
-            "@F", // truth value
-            "T",  // variable
+            "@T", // truth value
+            "@F",
+            "P", // logical variable
             "0 <= 1",
             "x+1 = y",
             "x <= y + 1",
-            "y + (x + 1) + z <= 0",
-            "1/2 x + 3 y <= 0",
+            "y + x + 1 + z <= 0",
+            "1/2 * x + 3 * y <= 0",
+            "(-1) * x = 0",
         ];
         for c in cases {
             assert!(grammer::AtomParser::new().parse(c).is_ok(), "case: {}", c);
@@ -67,7 +67,27 @@ mod test_parser {
 
     #[test]
     fn test_bad_atoms() {
-        let cases = vec!["@G", "x+1 ? y", "x > 0", "8 > 0", "1/2 x + 3 y >= 0"];
+        let cases = vec![
+            "@G",            // not a truth value
+            "x+1 ? y",       // ? not a relation
+            "(x+1) = y",     // parenthesized sums are not supported (not needed yet)
+            "2 * (x+1) = y", // multiplying composite terms not supported (yet)
+            "(x + 1) * y",   // non-linear
+        ];
+        for c in cases {
+            assert!(grammer::AtomParser::new().parse(c).is_err(), "case: {}", c);
+        }
+    }
+
+    ///  TODO: support all these cases
+    #[test]
+    fn test_atoms_not_yet_supported() {
+        let cases = vec![
+            "1/2 * x + 3 * y >= 0", // >= isn't supported yet, only <=
+            "x > 0",                // strict ineq not supported yet
+            "8 > 0",                // constant relations not supported
+            "0 <= .123",            // decimals not supported
+        ];
         for c in cases {
             assert!(grammer::AtomParser::new().parse(c).is_err(), "case: {}", c);
         }
@@ -101,7 +121,7 @@ mod test_parser {
     fn parse_big_rat() {
         let big_rat_good = "922337203685477580700 / 3"; // numerator is 100 * i64::MAX
         assert_eq!(
-            grammer::TermParser::new().parse(big_rat_good),
+            grammer::SumParser::new().parse(big_rat_good),
             Ok(Term::Num(rbig!(922337203685477580700 / 3)))
         );
 
