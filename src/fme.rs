5059e8040626856ab180936ee1df286a96a0cd48
Benjamin F Jones <benjaminfjones@gmail.com>
2026-04-04 13:59:34 -0700

    Start implementation of FME solver; systems of equations only

--- src/fme.rs
+++ src/fme.rs
@@ -1,8 +1,72 @@
 //! Implementation of Fourier-Motzkin Elimination
 //! <https://en.wikipedia.org/wiki/Fourier%E2%80%93Motzkin_elimination>
 
+use crate::{lin_rel::LinRel, lin_sys::LinSys};
+
+/// FME Solver State
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum FMEState {
+    /// `check` has not been called
+    UNKNOWN,
+    /// system of (in)equalities is satisfiable
+    SAT,
+    /// system of (in)equalities is unsatisfiable
+    UNSAT,
+}
+
+#[derive(Debug)]
+pub struct FMESolver {
+    state: FMEState,
+    system: LinSys,
+}
+
+impl FMESolver {
+    /// Create a fresh solver
+    pub fn new() -> Self {
+        Self {
+            state: FMEState::UNKNOWN,
+            system: LinSys::new(),
+        }
+    }
+
+    /// Assert a new relation
+    pub fn assert(&mut self, rel: LinRel) {
+        self.system.add_relation(rel);
+    }
+
+    /// Reset the solver state and clear all assertions
+    pub fn reset(&mut self) {
+        self.state = FMEState::UNKNOWN;
+        self.system.clear();
+    }
+
+    /// Check satisfiablility in the current state
+    pub fn check(&mut self) -> FMEState {
+        loop {
+            // reduce system to inequalities and constant equalities
+            self.system.eliminate_nontrivial_eqs();
+            // remove the trivial equalities
+            self.system.eliminate_trivial_eqs();
+            // check there are no contradictory (in)equalities
+            if self.system.has_trivial_contradiction() {
+                return FMEState::UNSAT;
+            }
+            // If after equality removal there were only trivially SAT equalities then
+            // the original system is SAT.
+            if self.system.relations().is_empty() {
+                return FMEState::SAT;
+            }
+            break;
+        }
+        todo!()
+    }
+}
+
 #[cfg(test)]
 mod test_fme {
+    use super::*;
+
+    use crate::fme::FMESolver;
     use crate::lin_expr::{Bound, LinExpr, LinExprBound};
     use crate::lin_rel::LinRel;
     use crate::lin_sys::LinSys;
@@ -45,5 +109,21 @@ mod test_fme {
                 expr: LinExpr::new(vec![rbig!(-5 / 10), rbig!(0), rbig!(0), rbig!(0)]).unwrap(),
             }
         )
+    }
+
+    #[test]
+    fn test_solver_check_only_equalities_2eq_sat() {
+        let mut solver = FMESolver::new();
+        solver.assert(eq!(0, 1, 1)); // x1 + x2 = 0
+        solver.assert(eq!(0, 0, 1)); // x2 = 0
+        assert_eq!(solver.check(), FMEState::SAT);
+    }
+
+    #[test]
+    fn test_solver_check_only_equalities_2eq_unsat() {
+        let mut solver = FMESolver::new();
+        solver.assert(eq!(1, 1)); // 1 + x1 = 0
+        solver.assert(eq!(2, 1)); // 2 + x1 = 0
+        assert_eq!(solver.check(), FMEState::UNSAT);
     }
 }
